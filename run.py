333875b09e35e388167d916cf13d68ed2fb9ab73
salem ALmotiry <60990492+salemALmotiry@users.noreply.github.com>
2022-05-07 17:50:46 +0300

    r

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+import pathlib
 from app import app 
 
 if __name__ == "__main__":
+     print(pathlib.Path("run.py").parent.absolute())
+     print(pathlib.Path("run.py").parent.resolve())
+
      app.run()
 
