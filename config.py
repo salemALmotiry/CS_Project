8dcef63e0ae5c5a218257c75b3cef581e09fb083
salem ALmotiry <60990492+salemALmotiry@users.noreply.github.com>
2022-05-07 17:43:55 +0300

    r

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ class Config(object):
 
     SECRET_KEY = "B\xb2?.\xdf\x9f\xa7m\xf8\x8a%,\xf7\xc4\xfa\x91"
 
-    UPLOAD_PATH= r"app/CS_Project/upload"
-    RSA_PATH= r"App/CS_Project/rsa_files"
+    UPLOAD_PATH= r"/app/CS_Project/upload"
+    RSA_PATH= r"/app/CS_Project/rsa_files"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     
     PRIVATE_KEY={
