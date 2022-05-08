05898934a6e33d48e00c3ab1ec3ceafd0b9f9c41
salem ALmotiry <60990492+salemALmotiry@users.noreply.github.com>
2022-05-08 03:04:00 +0300

    r

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 
+from re import T
+from time import sleep
 from app import app 
 
 if __name__ == "__main__":
      
-
-     app.run()
+  while True : 
+     try :
+        app.run()
+     except: 
+          sleep(1)
 
